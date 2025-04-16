4eb9fe85d7bab1cbfc1ef5e536686c06fe0b9cc6
Sofloflo <santiflames@gmail.com>
2025-04-15 20:37:41 -0400

    Add files via upload

--- #CarFinder v0.2.py
+++ #CarFinder v0.2.py
@@ -12,9 +12,11 @@ print("2. SEARCH for Authorized Vehicle")
 print("3. Exit\n")
 
 response = int(input("Enter here: "))
-print("\n")
 
-if (response == 1):
+while(True):
+
+     if (response == 1):
+        print("\n")
         print(f'The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:')
         print("\n")
         for answer in allowedVehiclesList:
@@ -27,30 +29,43 @@ if (response == 1):
         print("1. PRINT all Authorized Vehicles")
         print("2. SEARCH for Authorized Vehicle")
         print("3. Exit\n")
+        response = int(input("Enter here: "))
+        print("\n")
+        continue;
 
-if(response == 2):
-    search = input("Please Enter the full Vehicle name: ")
-    if (search == "Ford F-150"):
-         print("Ford F-150 is an authorized Vehicle")
-         print("\n")
-         print("**********************************")
-         print("AutoCountry Vehicle Finder v0.1")
-         print("**********************************\n")
-         print("Please Enter the following number below from the following menu:\n")
-         print("1. PRINT all Authorized Vehicles")
-         print("2. SEARCH for Authorized Vehicle")
-         print("3. Exit\n")
-    if (search == "Tesla"):
-         print("Tesla is not an authorized vehicle, if you received this in error please check the spelling and try again")
-         print("\n")
-         print("**********************************")
-         print("AutoCountry Vehicle Finder v0.1")
-         print("**********************************\n")
-         print("Please Enter the following number below from the following menu:\n")
-         print("1. PRINT all Authorized Vehicles")
-         print("2. SEARCH for Authorized Vehicle")
-         print("3. Exit\n")
-          
+     if(response == 2):
+          print("\n")
+          search = input("Please Enter the full Vehicle name: ")
+          if search in allowedVehiclesList:
+               print("\n")
+               print(f'{search} is an authorized vehicle')
+               print("\n")
+               print("**********************************")
+               print("AutoCountry Vehicle Finder v0.1")
+               print("**********************************\n")
+               print("Please Enter the following number below from the following menu:\n")
+               print("1. PRINT all Authorized Vehicles")
+               print("2. SEARCH for Authorized Vehicle")
+               print("3. Exit\n")
+               response = int(input("Enter here: "))
+               print("\n")
+               continue;
+          else: 
+               print(f'{search} is not an authorized vehicle, if you received this in error please check the spelling and try again')
+               print("\n")
+               print("**********************************")
+               print("AutoCountry Vehicle Finder v0.1")
+               print("**********************************\n")
+               print("Please Enter the following number below from the following menu:\n")
+               print("1. PRINT all Authorized Vehicles")
+               print("2. SEARCH for Authorized Vehicle")
+               print("3. Exit\n")
+               response = int(input("Enter here: "))
+               print("\n")
+               continue;
+     if (response == 3):
+          print("\n")
+          print( "Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+          break;
 
-if (response == 3):
-    print( "Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+     
